2a7c03513c73f34b05f7fb7b6517b2c03a78fe99
DevNewbie1826 <DevNewbie1826@users.noreply.github.com>
2026-09-04 21:31:48 +0900

    style(omorpc): gofmt windows lock files

--- internal/omorpc/ensure_lock_windows.go
+++ internal/omorpc/ensure_lock_windows.go
@@ -26,7 +26,7 @@ func openAndFlockEnsureLock(path string) (*os.File, error) {
 		windows.FILE_SHARE_READ|windows.FILE_SHARE_WRITE|windows.FILE_SHARE_DELETE,
 		nil,
 		windows.OPEN_ALWAYS,
-		windows.FILE_ATTRIBUTE_NORMAL | windows.FILE_FLAG_BACKUP_SEMANTICS |
+		windows.FILE_ATTRIBUTE_NORMAL|windows.FILE_FLAG_BACKUP_SEMANTICS|
 			windows.FILE_FLAG_OPEN_REPARSE_POINT,
 		0)
 	if err != nil {
